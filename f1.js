5f61a63eb8792a7a936c665aa69b41f11c80594b
2020jschultz <42749630+2020jschultz@users.noreply.github.com>
2018-09-27 13:47:58 -0500

    Add files via upload
    done

---
+++ f1.js
@@ -0,0 +1,168 @@
+/*
+** This is the file f1.js
+** Read each comment below, declare each function.
+** These are all PURE FUNCTIONS, they only depend and change the input values.
+*/
+
+
+/*******************************NUMBER FOCUSED****************************/
+
+// (1) First Task
+// Declare a function named double() which outputs a number double the input number.
+function double(a){
+    let total = a*2;
+    return total;
+}
+
+
+
+
+
+
+
+
+// (2) Second Task
+// Declare a function named plusplus() which outputs a number one higher than the input number.
+function plusplus(a){
+    let total = a+1;
+    return total;
+
+}
+
+
+
+
+
+
+
+// (3) Third Task
+// Declare a function named oppRecip() which outputs the opposite reciprocal of the input number.
+function oppRecip(a){
+    let total = -1/a;
+    return total;
+}
+
+
+
+
+
+
+// (4) Fourth Task
+// Declare a function named pthagorean() which has two inputs you guessed it a and b.
+// The function outputs the value of the third side of the right triangle.
+function pythagorean(a,b){
+    let c = Math.sqrt(a*a + b*b);
+    return c;
+}
+
+
+
+
+
+
+// (5) Fifth Task
+// Declare a function named circleArea() which has one input named radius.
+// The function outputs the area of the circle.
+function circleArea(radius){
+    let area = radius * radius * Math.PI;
+    return area;
+}
+
+
+
+
+
+
+// (6) Sixth Task
+// Declare a function named boxVolume() which has three inputs: length, width, and depth.
+// The function outputs the volume of the box;
+function boxVolume(length, width, depth){
+    let volume = length * width * depth;
+    return volume;
+}
+
+
+
+
+
+
+/**************************** STRING FOCUSED *********************************/
+
+
+
+// (7) Seventh Task
+// Declare a function named thisIsMadness() which accepts one string input named city.
+// The function should output the string: "No! This is <city>!!"
+function thisIsMadness(city){
+    return "No!" + "This is" + city;
+}
+
+
+
+
+
+
+// (8) Eigth Task
+// Declare a function named yo() which accepts one string input named fam.
+// The function should output the string: "yo <fam>!  What's good <fam>??"
+function yo(fam){
+    return "yo" + fam + "!" + "What's good" + fam + "??";
+}
+
+
+
+
+
+
+
+// (9) Ninth Task
+// Declare a function named beefDay() which has no inputs.
+// This function should output a long string, IN ALL CAPS, of "BEEF DAY" with lots of extra E's, F's, A's, and Y's
+function beefDay(){
+    return "BEEEEEEFFFFF DAAAAAAYYYYY";
+}
+
+
+
+
+
+
+
+/**************************** BOOLEAN FOCUSED *******************************/
+
+
+// (10) Tenth Task
+// Declare a function named xor() which has two inputs a and b.
+// This function returns the exclusive or of the two boolean inputs.
+function xor(a,b){
+    return ((a && !b ) ||( !a && b ));
+}
+
+
+
+
+
+
+
+
+
+/**************************** END OF FILE *******************************/
+function main(){
+    let x = 5;
+    console.log(`For the next few examples let x = ${x}`);
+    console.log(`1. double the number = ${double(x)}`);
+    console.log(`2. adding one to the number = ${plusplus(x)}`);
+    console.log(`3. the opposite reciprocal of ${x} is ${oppRecip(x)}`);
+    console.log(`4. the pythagorean theorem does 5 = ${pythagorean(3,4)}?`);
+    console.log(`5. the circle with radius ${x} has area = ${circleArea(x)}`);
+    console.log(`6. the volume of a ${x}, ${2*x}, ${3*x} box has volume = ${boxVolume(x,2*x,3*x)}`);
+    console.log(`7. ${thisIsMadness("SPARTA")}`);
+    console.log(`8. ${yo("dude")}`);
+    console.log(`9. ${beefDay()}`);
+    console.log(`10. first test: true == ${xor(true,false)} == ${xor(false,true)}`);
+    console.log(`10. second test: false == ${xor(true,true)} == ${xor(false,false)}`);
+  
+  }
+  
+  //execution of main function
+  main();
